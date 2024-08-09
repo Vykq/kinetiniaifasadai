db6c1a13de8c322ee924a2c5e8d542e77f32a905
Vykintas <vykintas.venckus@gmail.com>
2024-08-09 09:36:29 +0300

    first revision

--- blocks/main-hero.php
+++ blocks/main-hero.php
@@ -2,7 +2,7 @@
     <div class="container">
         <?php if(get_field('logo_video','main-settings')) : ?>
             <div class="logo-area mobile">
-                <video class="logo-video"playsinline muted loop autoplay poster="<?php echo get_field('logo','main-settings'); ?>">
+                <video class="logo-video" playsinline muted loop autoplay poster="<?php echo get_field('logo','main-settings'); ?>">
                     <source src="<?php echo get_field('logo_video','main-settings'); ?>" type="video/mp4">
                 </video>
             </div>
@@ -17,7 +17,7 @@
             <div class="left">
                 <?php if(get_field('logo_video','main-settings')) : ?>
                     <div class="logo-area desktop">
-                        <video class="logo-video"playsinline muted loop autoplay poster="<?php echo get_field('logo','main-settings'); ?>">
+                        <video class="logo-video" playsinline muted loop autoplay poster="<?php echo get_field('logo','main-settings'); ?>">
                             <source src="<?php echo get_field('logo_video','main-settings'); ?>" type="video/mp4">
                         </video>
                     </div>
@@ -39,8 +39,8 @@
                         <?php if(get_field('more_text')) : ?>
                             <span class="tooltip" id="tippy-1-left">
                                 <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <circle cx="10.5" cy="10.5" r="10.5" fill="#3D7FFF"/>
-                                    <path d="M12.0741 14.3384C12.0741 14.5795 12.1879 14.7201 12.4156 14.7603L13.199 14.9009V15.9454H8.29776V14.9009L9.08115 14.7603C9.3088 14.7201 9.42262 14.5795 9.42262 14.3384V9.37696C9.42262 9.13592 9.31549 8.99531 9.10123 8.95513L8.21741 8.77435V7.68966C8.73967 7.54235 9.32219 7.43522 9.96497 7.36826C10.6078 7.30131 11.1903 7.26783 11.7125 7.26783L12.0741 7.58922V14.3384ZM12.054 4.95783C12.054 5.35957 11.9201 5.68096 11.6523 5.922C11.3978 6.16305 11.0564 6.28357 10.6278 6.28357C10.1859 6.28357 9.83106 6.16305 9.56323 5.922C9.3088 5.68096 9.18158 5.35957 9.18158 4.95783C9.18158 4.55609 9.3088 4.24139 9.56323 4.01374C9.83106 3.7727 10.1859 3.65218 10.6278 3.65218C11.0564 3.65218 11.3978 3.7727 11.6523 4.01374C11.9201 4.24139 12.054 4.55609 12.054 4.95783Z" fill="white"/>
+                                <circle cx="10.5" cy="10.5" r="10.5" fill="#DBFF00"/>
+                                <path d="M12.074 14.3384C12.074 14.5795 12.1878 14.7201 12.4155 14.7603L13.1989 14.9009V15.9454H8.29763V14.9009L9.08102 14.7603C9.30868 14.7201 9.4225 14.5795 9.4225 14.3384V9.37696C9.4225 9.13592 9.31537 8.99531 9.10111 8.95513L8.21729 8.77435V7.68966C8.73955 7.54235 9.32207 7.43522 9.96485 7.36826C10.6076 7.30131 11.1902 7.26783 11.7124 7.26783L12.074 7.58922V14.3384ZM12.0539 4.95783C12.0539 5.35957 11.92 5.68096 11.6522 5.922C11.3977 6.16305 11.0562 6.28357 10.6277 6.28357C10.1858 6.28357 9.83094 6.16305 9.56311 5.922C9.30868 5.68096 9.18146 5.35957 9.18146 4.95783C9.18146 4.55609 9.30868 4.24139 9.56311 4.01374C9.83094 3.7727 10.1858 3.65218 10.6277 3.65218C11.0562 3.65218 11.3977 3.7727 11.6522 4.01374C11.92 4.24139 12.0539 4.55609 12.0539 4.95783Z" fill="black"/>
                                 </svg>
                             </span>
                         <?php endif; ?>
